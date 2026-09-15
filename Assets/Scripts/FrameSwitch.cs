622ba871ec237c73c038bf3a53895e23fb1b7cb4
SlavicMag <govoruxadaniil@gmail.com>
2026-09-15 05:41:44 +0300

    Разобрался с локацией

---
+++ Assets/Scripts/FrameSwitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class FrameSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject activeFrame;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            activeFrame.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            activeFrame.SetActive(false);
+        }
+    }
+}
